fd9e2d685e52c473f142ada1ef43f11db7f09c12
Octavian Geagla <ogeagla@gmail.com>
2014-06-13 08:46:01 -0600

    Few changes to try out compile-time checks for multi-dim arrays using repa

--- library/Quasidiffusion.hs
+++ library/Quasidiffusion.hs
@@ -1,5 +1,8 @@
 -- | Quasidiffusion module.
 module Quasidiffusion (quasidiffusion) where
+import qualified Data.Array.Repa as R
+import qualified Data.Array.Repa.IO.BMP as RIOB
+import qualified Data.Array.Repa.IO.Matrix as RIOM
 
 {- |
      This contains blah blah blah
@@ -10,14 +13,32 @@ module Quasidiffusion (quasidiffusion) where
 quasidiffusion :: ()
 quasidiffusion = ()
 
+type DIM6 = R.DIM5  R.:.  R.Z
+type DIM7 =   DIM6  R.:.  R.Z
+type DIM8 =   DIM7  R.:.  R.Z
+type DIM9 =   DIM8  R.:.  R.Z
+type DIM10 =  DIM9  R.:.  R.Z
+type DIM11 =  DIM10 R.:.  R.Z
 
-{-import Data.Array
-import Data.List (sortBy)
 
-type VFieldElem = Float
-type VField1 = Array (Int) VFieldElem
-type VField2 = Array (Int) VField1
-type VField3 = Array (Int) VField2
+phi :: R.Array R.U R.DIM1 Double
+phi = R.fromListUnboxed (R.Z R.:. (3::Int)) [1.0..3.0]
+xt = R.extent phi
+
+inputs = [1.0..10.0] :: [Double]
+phi2 :: R.Array R.U R.DIM2 Double
+phi2 = R.fromListUnboxed (R.Z R.:. (5::Int) R.:. (5::Int)) inputs
+
+
+{-
+Prelude Data.Array.Repa.IO.Matrix> :t writeMatrixToTextFile 
+writeMatrixToTextFile
+  :: (Show e, repa-3.2.3.3:Data.Array.Repa.Base.Source r e) =>
+     FilePath
+     -> repa-3.2.3.3:Data.Array.Repa.Base.Array
+          r Data.Array.Repa.Index.DIM2 e
+     -> IO ()
 -}
---phi :: VField3
---phi = zeros // [[
+
+writeData = RIOM.writeMatrixToTextFile "test.dat" phi2
+
